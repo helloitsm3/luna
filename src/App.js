0d9cd46831c0e559c7d1b62a39178d0b852ec3e0
Peter (Cytronicode) <92656878+Cytronicode@users.noreply.github.com>
2021-11-13 12:02:00 +0000

    Added instructions on how to donate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import instagramLogo from './assets/instagram-logo.svg';
-import soliloquyLogo from './assets/soliloquy-logo.svg';
+import soliloquyOutline from './assets/soliloquy-outline.svg';
 import './App.css';
 
 // Constants
 const INSTAGRAM_HANDLE = 'cytronical.eth';
 const INSTAGRAM_LINK = `https://instagram.com/${INSTAGRAM_HANDLE}`;
 
+const DEVELOPER_ADDRESS = 'HhK2kzyUnpg1CDD9dj6JqYPP5auWj22mdiQ45jmvdRgh';
+
 const App = () => {
+
+  const sendSolToDev = () => (
+    <p className="sub-text"><b>Want to have a voice on what we are building next?</b><br />1. Donate some SOL to <code>{DEVELOPER_ADDRESS}</code><br />2. DM Cytronical.eth#4975 your address to become a patreon!</p>
+  );
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <img alt="Soliloquy Logo" className="logo-svg-big" src={soliloquyLogo} />
+          <img alt="Soliloquy Logo" className="logo-svg-big" src={soliloquyOutline} />
           <p className="header">Soliloquy</p>
           <p className="sub-text">
-            Chat in realtime with the Solana blockchain.
+            Chat in realtime with the Solana blockchain!
           </p>
+          {sendSolToDev()}
+          <button className="cta-button connect-wallet-button">Coming soon!</button>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="logo-svg" src={instagramLogo} />
